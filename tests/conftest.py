42d3ed2a70ccbb1a2e70e23a806babaae9743ea9
kumapo <kumapo@users.noreply.github.com>
2018-12-10 12:29:48 +0900

    Use parametrize for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,9 +18,39 @@ def mocked_client(client):
         # create a table for the stub
         table = conn.create_table(
             TableName="chats",
-            KeySchema=[{'AttributeName':'id','KeyType':'HASH'}],
-            AttributeDefinitions=[{'AttributeName':'id','AttributeType':'S'}],
+            KeySchema=[{'AttributeName':'message_id','KeyType':'HASH'}],
+            AttributeDefinitions=[{'AttributeName':'message_id','AttributeType':'S'}],
             ProvisionedThroughput={'ReadCapacityUnits':5,'WriteCapacityUnits':5})
+        # run a testcase in stubbed context
+        yield client
+
+@pytest.fixture()
+def mocked_client_having_messages_in_chat_literally(client):
+    # use stub for dynamo db
+    with mock_dynamodb2():
+        conn = boto3.resource('dynamodb')
+        # create a table for the stub
+        table = conn.create_table(
+            TableName="chats",
+            KeySchema=[{'AttributeName':'message_id','KeyType':'HASH'}],
+            AttributeDefinitions=[{'AttributeName':'message_id','AttributeType':'S'}],
+            ProvisionedThroughput={'ReadCapacityUnits':5,'WriteCapacityUnits':5})
+        # build and insert items into the table
+        chat = [{
+            'id': 'ff86c522-a08e-4a2c-a222-80e62c9c059b',
+            'message_id': '0f64eaf2-fcbe-a1cb-0b88-c27234598fde',
+            'message': 'What is the most important thing?',
+            'message_date': '2018-12-05T13:28:18.175895',
+            'username': 'ksr' 
+        }, {
+            'id': 'ff86c522-a08e-4a2c-a222-80e62c9c059b',
+            'message_id': '199ec6c4-fff7-12c9-0bf9-a1622a2712dd',
+            'message': "Security",
+            'message_date': '2018-12-05T13:29:37.132019',
+            'username': 'bro' 
+        }]
+
+        _ = [table.put_item(Item=message) for message in chat]
         # run a testcase in stubbed context
         yield client
 
@@ -32,41 +62,33 @@ def mocked_client_having_messages_in_chat(client):
         # create a table for the stub
         table = conn.create_table(
             TableName="chats",
-            KeySchema=[{'AttributeName':'id','KeyType':'HASH'}],
-            AttributeDefinitions=[{'AttributeName':'id','AttributeType':'S'}],
+            KeySchema=[{'AttributeName':'message_id','KeyType':'HASH'}],
+            AttributeDefinitions=[{'AttributeName':'message_id','AttributeType':'S'}],
             ProvisionedThroughput={'ReadCapacityUnits':5,'WriteCapacityUnits':5})
         # build and insert items into the table
-        messsages = tests.factory.ChatFactory.dict_factory().build_batch(5, id='ff86c522-a08e-4a2c-a222-80e62c9c059b')
-        _ = [table.put_item(Item=messsage) for messsage in messsages]
+        chats = tests.factory.ChatFactory.dict_factory().build_batch(5, id='ff86c522-a08e-4a2c-a222-80e62c9c059b')
+        _ = [table.put_item(Item=chat) for chat in chats]
         # run a testcase in stubbed context
         yield client
 
 @pytest.fixture()
-def mocked_client_having_messages_in_chat_manually(client):
-    # use stub for dynamo db
+def messages_in_chat():
+    return { 'chat_id':'ef86c522-a08e-4a2c-a222-80e62c9c059c',
+             'n_messages':6 }
+
+@pytest.fixture()
+def mocked_client_having_messages_in_chat_parametrically(client, messages_in_chat):    # use stub for dynamo db
     with mock_dynamodb2():
         conn = boto3.resource('dynamodb')
         # create a table for the stub
         table = conn.create_table(
             TableName="chats",
-            KeySchema=[{'AttributeName':'id','KeyType':'HASH'}],
-            AttributeDefinitions=[{'AttributeName':'id','AttributeType':'S'}],
+            KeySchema=[{'AttributeName':'message_id','KeyType':'HASH'}],
+            AttributeDefinitions=[{'AttributeName':'message_id','AttributeType':'S'}],
             ProvisionedThroughput={'ReadCapacityUnits':5,'WriteCapacityUnits':5})
         # build and insert items into the table
-        messages = [{
-            'id': 'ff86c522-a08e-4a2c-a222-80e62c9c059b',
-            'message_id': '0f64eaf2-fcbe-a1cb-0b88-c27234598fde',
-            'message': 'What do you dislike about the city?',
-            'message_date': '2018-12-05T13:28:18.175895',
-            'username': 'bra' 
-        }, {
-            'id': 'ff86c522-a08e-4a2c-a222-80e62c9c059b',
-            'message_id': '199ec6c4-fff7-12c9-0bf9-a1622a2712dd',
-            'message': "Security",
-            'message_date': '2018-12-05T13:29:37.132019',
-            'username': 'bro' 
-        }]
-
-        _ = [table.put_item(Item=message) for message in messages]
+        chat = tests.factory.ChatFactory.dict_factory().build_batch(messages_in_chat['n_messages'], 
+                                                                    id=messages_in_chat['chat_id'])        
+        _ = [table.put_item(Item=messsage) for messsage in chat]
         # run a testcase in stubbed context
         yield client
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,25 +1,32 @@
 import pytest
 import json
 
-MESSAGES_IN_A_CHAT=5
-CHAT_ID='ff86c522-a08e-4a2c-a222-80e62c9c059b'
-
 def test_post_chats(mocked_client):
 	# fixtures
 	messages = [{
 		'message_id': '9f64eaf2-ecbe-41cb-ab88-b27234598fde',
-		'message': 'Where do you stay?',
+		'message': 'Have ever been to NYC?',
 		'message_date': '2018-12-05T13:26:28.175895',
-		'username': 'bra' 
+		'username': 'ksr' 
 	}, {
 		'message_id': '5f9ec6c4-44f7-43c9-bbf9-ed622a2712dd',
-		'message': "NYC",
+		'message': "Yes, I was born there",
 		'message_date': '2018-12-05T13:27:47.132019',
 		'username': 'bro' 
 	}]
 	res = mocked_client.post('/chats', json=messages)
 	assert len(res.json) == 2
 
+def test_get_messages_given_id_literally(mocked_client_having_messages_in_chat_literally):
+	res = mocked_client_having_messages_in_chat_literally.get('/chats/ff86c522-a08e-4a2c-a222-80e62c9c059b')
+	assert len(res.json) == 2
+
 def test_get_messages_given_id(mocked_client_having_messages_in_chat):
 	res = mocked_client_having_messages_in_chat.get('/chats/ff86c522-a08e-4a2c-a222-80e62c9c059b')
-	assert res.json is not None
+	assert len(res.json) == 5
+
+@pytest.mark.parametrize('messages_in_chat', [ { 'chat_id':'ef86c522-a08e-4a2c-a222-80e62c9c059b',
+												 'n_messages':12 }])
+def test_get_messages_given_messages_in_chat_parametrically(mocked_client_having_messages_in_chat_parametrically, messages_in_chat):
+	res = mocked_client_having_messages_in_chat_parametrically.get(f"/chats/{messages_in_chat['chat_id']}")
+	assert len(res.json) == messages_in_chat['n_messages']
